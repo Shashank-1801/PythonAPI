3bfba5965415f9e1a53a924cc09bb720dc3c7a35
Shashank Shekhar <shekhar@adobe.com>
2015-07-22 11:36:51 +0530

    adding one more except to handle mal-formed rows

--- hozifyAPI.py
+++ hozifyAPI.py
@@ -120,12 +120,16 @@ def findTag(tag):
         # Execute the SQL command
         data = cursor.fetchall()
         for x in data:
-            for p in (json.loads(x[0])['image_tags']):
-                try:
-                    if(tag == str(str(p['tag']))):
-                        result.append(x)
-                except:
-                    print "Data not found in this row"
+            try:
+                for p in (json.loads(x[0])['image_tags']):
+                    print (json.loads(x[0])['id'])
+                    try:
+                        if(tag == str(str(p['tag']))):
+                            result.append(x)
+                    except:
+                        print "Data not found in this row"
+            except:
+                print "Some error while processing DB row"
     except:
         print "Error in QUERY"
 
@@ -137,6 +141,7 @@ def findTag(tag):
 
 
 def main():
+    """
     pin_id = 366141
     url_json = getData(pin_id)
     url = extractUrl(url_json)
@@ -151,8 +156,9 @@ def main():
     #new_json = '{"id":366141,"parent_id":null,"board_id":26866,"category_id":36,"user_id":845,"date_added":"2015-03-31 00:55:31","date_modified":"2015-03-31 00:55:32","likes":8,"comments":0,"repins":0,"title":null,"description":"Earthy Minimal Living ","source_id":null,"from":null,"width":676,"height":1185,"image":"\/uploads\/pins\/2015\/03\/6b2a9998e3c301b60bdd206b6350cdb9.jpeg","store":"Local","store_host":"{local}","video":0,"background_color":null,"pinned_from":"Uploaded","gift":0,"price":null,"currency_code":null,"gallery":0,"status":1,"public":1,"liked":0,"user_is_follow":0,"board":"Living Room","username":"Pro_MayaPraxis","firstname":"mayaPRAXIS","lastname":"Design + Architecture","pins":25,"boards":8,"user_likes":0,"followers":0,"image_medium":{"image":"http:\/\/dev.houzify.com\/uploads\/pins\/2015\/03\/medium\/6b2a9998e3c301b60bdd206b6350cdb9.jpeg","width":236,"height":414},"image_big":{"image":"http:\/\/dev.houzify.com\/uploads\/pins\/2015\/03\/big\/6b2a9998e3c301b60bdd206b6350cdb9.jpeg","width":676,"height":1185},"user":{"id":845,"username":"Pro_MayaPraxis","password":"ceb11da7d9d833e059e71f804ac82432","password_new":null,"password_key":null,"email":"vijay@mayapraxis.com","firstname":"mayaPRAXIS","lastname":"Design + Architecture","pins":25,"likes":0,"comments":0,"boards":8,"followers":0,"following":0,"language_id":1,"status":1,"activate_url":null,"avatar_width":200,"avatar_height":208,"avatar":"\/uploads\/users\/2015\/01\/23d43e56fcd1c40bf262fb1b3332579b.jpeg","avatar_store_host":"{local}","avatar_store":"Local","date_added":"2015-01-21 05:08:57","date_modified":"2015-04-20 01:26:17","cover_width":0,"cover_height":0,"cover_top":0,"cover":null,"cover_store_host":null,"cover_store":null,"about":"Headed by Vijay Narnapatti and Dimple Mittal, MayaPraxis works on Architecture and related design fields. ","is_admin":0,"gender":"unsigned","status_send":null,"country_iso_code_3":"IND","website":"http:\/\/www.mayapraxis.biz","search_engines":1,"city":"Bangalore","first_login":1,"send_daily":1,"repins":0,"notification_comment_pin":1,"notification_mentioned":1,"notification_follow_user":1,"notification_like_pin":1,"notification_repin_pin":1,"notification_group_board":1,"notification_news":1,"last_online":"2015-05-15 04:26:20","activity_open":"2015-04-20 01:26:17","avatar_small":{"image":"http:\/\/dev.houzify.com\/uploads\/users\/2015\/01\/small\/23d43e56fcd1c40bf262fb1b3332579b.jpeg","width":60,"height":60},"avatar_medium":{"image":"http:\/\/dev.houzify.com\/uploads\/users\/2015\/01\/medium\/23d43e56fcd1c40bf262fb1b3332579b.jpeg","width":200,"height":200},"cover_image":{"width":986,"height":348,"bits":8,"mime":"image\/jpeg","extension":"jpeg","image":"http:\/\/dev.houzify.com\/uploads\/noimage\/usercovers\/small.jpeg"}}}'
     #print new_json
     writeDB(pin_id, new_json)
-    print (findTag('peace'))
-    print (findTag('abcasjcka'))
+    """
+    print (findTag('holiday'))
+    #print (findTag('abcasjcka'))
 
 if __name__ == '__main__':
     main()
