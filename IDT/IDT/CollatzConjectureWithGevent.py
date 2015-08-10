99bae2f7318a858194cf63d2ec853e33d1c78bd2
shekhar <shashankeshav@gmail.com>
2015-08-11 00:56:12 +0530

    making some more progress on gevent

--- IDT/IDT/CollatzConjectureWithGevent.py
+++ IDT/IDT/CollatzConjectureWithGevent.py
@@ -1,28 +1,20 @@
 import gevent
 from CollatzConjecture import calMaxCount
+from random import randint
 
 
 
 def newFunc(x,y, name):
-    gevent.sleep(3)
+    t = randint(0,5)*0.2
+    print name + "is sleeping for " + str(t)
+    gevent.sleep(t)
     print calMaxCount(x, y)
     print "End of function with name: " + str(name)
-
-def newFunc2(x,y, name):
-    print calMaxCount(x, y)
-    gevent.sleep(2)
-    print "End of function with name: " + str(name)
-
-def newFunc3(x,y, name):
-    print calMaxCount(x, y)
-    gevent.sleep(3)
-    print "End of function with name: " + str(name)
-
 
 
 g0 = gevent.spawn(newFunc,21, 30, "g0")    
-g1 = gevent.spawn(newFunc2,11, 20, "g1")
-g2 = gevent.spawn(newFunc3,1, 10, "g2")
+g1 = gevent.spawn(newFunc,11, 20, "g1")
+g2 = gevent.spawn(newFunc,1, 10, "g2")
 
 events = [g1,g2,g0]
 gevent.joinall(events)
