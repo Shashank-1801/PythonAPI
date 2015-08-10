0fece7c25424fa794c378fb8012831428ded1558
shekhar <shashankeshav@gmail.com>
2015-08-11 00:00:14 +0530

    adiing examples of greenlet and gevent

---
+++ IDT/IDT/extra1.py
@@ -0,0 +1,29 @@
+from greenlet import greenlet
+'''
+def test1():
+    print 12
+    gr2.switch()
+    print 34
+
+def test2():
+    print 56
+    gr1.switch()
+    print 78
+
+gr1 = greenlet(test1)
+gr2 = greenlet(test2)
+gr1.switch()
+
+'''
+
+def test1(x, y):
+    z = gr2.switch(x+y)
+    print z
+
+def test2(u):
+    print u
+    gr1.switch(42)
+
+gr1 = greenlet(test1)
+gr2 = greenlet(test2)
+gr1.switch("hello", " world")
